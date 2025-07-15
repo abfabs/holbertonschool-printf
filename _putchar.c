debf8ba8bf3e5cf2cc88a55a1ca2c5b6ff1c09ce
abfabs <albaeftimi@gmail.com>
2025-07-15 08:46:56 +0000

    alba push

--- _putchar.c
+++ _putchar.c
@@ -3,10 +3,10 @@
 /**
  * _putchar - Writes a character to standard output
  * @c: The character to print
- * 
+ *
  * Return: 1 on success, -1 on error (and sets errno)
  */
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
