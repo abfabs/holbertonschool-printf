8294e0daded5e09558836b89d1bb78c9e21e17f8
abfabs <albaeftimi@gmail.com>
2025-07-15 08:48:10 +0000

    alba push

--- main.h
+++ main.h
@@ -1,22 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
+
 /**
- * struct specifier_t - Struct for format specifiers and their handlers
- * @specifier: The format specifier (e.g., "c", "s")
- * @handler: Function pointer to the handler function for the specifier
+ * struct specifier - Struct to match format with handler
+ * @specifier: The format string
+ * @handler: The function to handle the format
  */
-typedef struct specifier_t
+typedef struct specifier
 {
-    char *specifier;
-    int (*handler)(va_list args);
+	char *specifier;
+	int (*handler)(va_list);
 } specifier_t;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
-int (*find_handler(char format_char, specifier_t specifiers[]))(va_list);
-int process_format(const char *format, va_list args, specifier_t specifiers[]);
 int handle_char(va_list args);
 int handle_string(va_list args);
-int handle_int(va_list args);
-int print_number(long int n);
-#endif
+
+#endif /* MAIN_H */
