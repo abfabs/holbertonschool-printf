b3c479221d1efae8d27ea0a9438697b3c34199d1
abfabs <albaeftimi@gmail.com>
2025-07-15 08:45:30 +0000

    alba push

--- _printf.c
+++ _printf.c
@@ -1,88 +1,101 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
 #include <stddef.h>
 
-
+/**
+ * find_handler - Finds the correct format handler.
+ * @format_char: The format specifier character.
+ * @specifiers: Array of specifier structs.
+ *
+ * Return: Pointer to handler function or NULL if not found.
+ */
 int (*find_handler(char format_char, specifier_t specifiers[]))(va_list)
 {
 	unsigned int i = 0;
-	
+
 	while (specifiers[i].specifier != NULL)
 	{
 		if (format_char == specifiers[i].specifier[0])
 			return (specifiers[i].handler);
 		i++;
 	}
-	return NULL;
+	return (NULL);
 }
 
-
-
-
+/**
+ * process_format - Processes the format string and prints output.
+ * @format: The format string.
+ * @args: Argument list.
+ * @specifiers: Array of specifier structs.
+ *
+ * Return: Number of characters printed.
+ */
 int process_format(const char *format, va_list args, specifier_t specifiers[])
 {
-    unsigned int i = 0;
-    int len = 0;
-    int (*handler)(va_list);
+	unsigned int i = 0;
+	int len = 0;
+	int (*handler)(va_list);
 
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%' && format[i + 1] != '\0')
-        {
-            if (format[i + 1] == '%')
-            {
-                _putchar('%');
-                len++;
-                i++;
-            }
-            else
-            {
-                handler = find_handler(format[i + 1], specifiers);
-                if (handler)
-                {
-                    len += handler(args);
-                    i++;
-                }
-                else
-                {
-                    _putchar('%');
-                    _putchar(format[i + 1]);
-                    len += 2;
-                    i++;
-                }
-            }
-        }
-        else
-        {
-            _putchar(format[i]);
-            len++;
-        }
-        i++;
-    }
-    return (len);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				len++;
+				i++;
+			}
+			else
+			{
+				handler = find_handler(format[i + 1], specifiers);
+				if (handler)
+				{
+					len += handler(args);
+					i++;
+				}
+				else
+				{
+					_putchar('%');
+					_putchar(format[i + 1]);
+					len += 2;
+					i++;
+				}
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			len++;
+		}
+		i++;
+	}
+	return (len);
 }
 
-
-
+/**
+ * _printf - Custom printf function.
+ * @format: The format string.
+ *
+ * Return: Number of characters printed, or -1 on error.
+ */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int len;
+	va_list args;
+	int len;
 
-    specifier_t specifiers[] = {
-        {"c", handle_char},
-        {"s", handle_string}, 
-        {NULL, NULL}
-    };
+	specifier_t specifiers[] = {
+		{"c", handle_char},
+		{"s", handle_string},
+		{NULL, NULL}
+	};
 
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-    va_start(args, format);
-    len = process_format(format, args, specifiers);
-    va_end(args);
+	va_start(args, format);
+	len = process_format(format, args, specifiers);
+	va_end(args);
 
-    return (len);
+	return (len);
 }
--- print_functions.c
+++ print_functions.c
@@ -1,36 +1,41 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
 
-
+/**
+ * handle_char - Handles the %c format specifier.
+ * @args: Argument list.
+ *
+ * Return: Number of characters printed (always 1).
+ */
 int handle_char(va_list args)
 {
-    char c;
+	char c;
 
-    c = va_arg(args, int);
+	c = va_arg(args, int);
+	_putchar(c);
 
-    _putchar(c);
-    
-    return (1);
+	return (1);
 }
 
-
+/**
+ * handle_string - Handles the %s format specifier.
+ * @args: Argument list.
+ *
+ * Return: Number of characters printed.
+ */
 int handle_string(va_list args)
-{   
-    unsigned int i;
-    char *str;
-    str = va_arg(args, char*);
+{
+	unsigned int i;
+	char *str;
 
-    if (str == NULL)
-    {
-        str = "(null)";
-    }
+	str = va_arg(args, char *);
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        _putchar(str[i]);
-    }
-    return (i);
+	if (str == NULL)
+		str = "(null)";
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	return (i);
 }
-
