090c5e88cf34dbf753f5c7d55455c5638c7ec6db
renisv <11432@holbertonstudents.com>
2025-07-15 08:10:26 +0000

    update

--- _printf.c
+++ _printf.c
@@ -13,3 +13,49 @@ int (*find_handler(char format_char, specifier_t specifiers[]))(va_list)
 	}
 	return NULL;
 }
+
+
+
+
+int process_format(const char *format, va_list args, specifier_t specifiers[])
+{
+    unsigned int i = 0;
+    int len = 0;
+    int (*handler)(va_list);
+
+    while (format[i] != '\0')
+    {
+        if (format[i] == '%' && format[i + 1] != '\0')
+        {
+            if (format[i + 1] == '%')
+            {
+                _putchar('%');
+                len++;
+                i++;
+            }
+            else
+            {
+                handler = find_handler(format[i + 1], specifiers);
+                if (handler)
+                {
+                    len += handler(args);
+                    i++;
+                }
+                else
+                {
+                    _putchar('%');
+                    _putchar(format[i + 1]);
+                    len += 2;
+                    i++;
+                }
+            }
+        }
+        else
+        {
+            _putchar(format[i]);
+            len++;
+        }
+        i++;
+    }
+    return (len);
+}
